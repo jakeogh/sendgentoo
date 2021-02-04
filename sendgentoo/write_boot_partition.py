a4b1fae8ef48151ca4d0888e8d5a33f187090dd1
jakeogh <github.com@v6y.net>
2021-02-03 18:06:34 -0700

    auto-commit

--- sendgentoo/write_boot_partition.py
+++ sendgentoo/write_boot_partition.py
@@ -6,6 +6,7 @@ from pathlib import Path
 import click
 from icecream import ic
 from kcl.commandops import run_command
+from kcl.deviceops import add_partition_number_to_device
 from kcl.deviceops import device_is_not_a_partition
 from kcl.deviceops import warn
 from kcl.mountops import block_special_path_is_mounted
@@ -32,10 +33,13 @@ def write_boot_partition(*,
         warn((device,))
 
     partition_number = '3'
+    partition = add_partition_number_to_device(device=device,
+                                               partition_number=partition_number,
+                                               verbose=verbose)
     start = "100MiB"
     end = "400MiB"
 
-    run_command("parted -a optimal " + device + " --script -- mkpart primary " + start + ' ' + end)
-    run_command("parted  " + device + " --script -- name " + partition_number + " bootfs")
-    run_command("mkfs.ext4 " + device + partition_number)
+    run_command("parted -a optimal " + device + " --script -- mkpart primary " + start + ' ' + end, verbose=True)
+    run_command("parted  " + device + " --script -- name " + partition_number + " bootfs", verbose=True)
+    run_command("mkfs.ext4 " + partition, verbose=True)
 

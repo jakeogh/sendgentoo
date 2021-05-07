cdef2943d9c6f2610f0e485227e2f24c5b2078eb
jakeogh <github.com@v6y.net>
2021-05-06 21:29:27 -0700

    auto-commit

--- sendgentoo/create_root_device.py
+++ sendgentoo/create_root_device.py
@@ -3,12 +3,12 @@
 from pathlib import Path
 
 import click
-from destroy_block_device import destroy_block_device_head_and_tail
-from kcl.deviceops import warn
-from kcl.deviceops import write_gpt
-from kcl.mountops import block_special_path_is_mounted
-from kcl.pathops import path_is_block_special
+from blocktool import destroy_block_device_head_and_tail
+from blocktool import path_is_block_special
+from blocktool import warn
+from blocktool import write_gpt
 from kcl.printops import eprint
+from mounttool import block_special_path_is_mounted
 
 from .setup_globals import RAID_LIST
 from .write_sysfs_partition import write_sysfs_partition
@@ -44,11 +44,11 @@ def create_root_device(ctx,
         if not Path(device).name.startswith('nvme'):
             assert not device[-1].isdigit()
         assert path_is_block_special(device)
-        assert not block_special_path_is_mounted(device)
+        assert not block_special_path_is_mounted(device, verbose=verbose, debug=debug,)
 
     #assert os.getcwd() == '/home/cfg/setup/gentoo_installer'
     if not force:
-        warn(devices)
+        warn(devices, verbose=verbose, debug=debug,)
 
     if exclusive:
         if filesystem != 'zfs':
@@ -68,25 +68,20 @@ def create_root_device(ctx,
         pass
 
     if pool_name:
-        ctx.invoke(write_sysfs_partition, devices=devices, force=True, exclusive=exclusive, filesystem=filesystem, raid=raid, pool_name=pool_name, raid_group_size=raid_group_size)
+        ctx.invoke(write_sysfs_partition,
+                   devices=devices,
+                   force=True,
+                   exclusive=exclusive,
+                   filesystem=filesystem,
+                   raid=raid,
+                   pool_name=pool_name,
+                   raid_group_size=raid_group_size,)
     else:
-        ctx.invoke(write_sysfs_partition, devices=devices, force=True, exclusive=exclusive, filesystem=filesystem, raid=raid, raid_group_size=raid_group_size)
-
-
-#@click.command()
-#@click.argument('devices',         required=True, nargs=-1)
-#@click.option('--partition-table', is_flag=False, required=True, type=click.Choice(['gpt']))
-#@click.option('--filesystem',      is_flag=False, required=True, type=click.Choice(['ext4', 'zfs']))
-#@click.option('--force',           is_flag=True,  required=False)
-#@click.option('--exclusive',       is_flag=True,  required=False)
-#@click.option('--raid',            is_flag=False, required=True, type=click.Choice(RAID_LIST))
-#@click.option('--raid-group-size', is_flag=False, required=True, type=int)
-#@click.option('--pool-name',       is_flag=False, required=False, type=str)
-#@click.pass_context
-#def main(ctx, devices, partition_table, filesystem, force, exclusive, raid, raid_group_size, pool_name):
-#    create_root_device(ctx, devices=devices, partition_table=partition_table, filesystem=filesystem, force=force, exclusive=exclusive, raid=raid, raid_group_size=raid_group_size, pool_name=pool_name)
-#
-#
-#if __name__ == '__main__':
-#    main()
+        ctx.invoke(write_sysfs_partition,
+                   devices=devices,
+                   force=True,
+                   exclusive=exclusive,
+                   filesystem=filesystem,
+                   raid=raid,
+                   raid_group_size=raid_group_size,)
 

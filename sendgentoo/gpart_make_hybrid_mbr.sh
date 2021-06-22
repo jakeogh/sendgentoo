add3d128596cc0bfca271ff6dad4f25868b67836
jakeogh <github.com@v6y.net>
2021-06-21 21:20:10 -0700

    auto-commit

--- sendgentoo/gpart_make_hybrid_mbr.sh
+++ sendgentoo/gpart_make_hybrid_mbr.sh
@@ -5,9 +5,9 @@ usage="boot_device"
 test "$#" -eq "${argcount}" || { echo "$0 ${usage}" > /dev/stderr && exit 1 ; } #"-ge=>=" "-gt=>" "-le=<=" "-lt=<" "-ne=!="
 
 device="${1}"
-test -b "${device}" || { echo "${device} not found or is not a block device. Exiting." > /dev/stderr ; exit 1 ; }
-mount | grep "${device}" && { echo "${device} is mounted. Exiting." ; exit 1 ; }
+test -b "${device}" || { echo "$0: ${device} not found or is not a block device. Exiting." > /dev/stderr ; exit 1 ; }
+mount | grep "${device}" && { echo "$0: ${device} is mounted. Exiting." ; exit 1 ; }
 
-echo "making hybrid MBR"
+echo "$0: making hybrid MBR"
 /home/cfg/_myapps/sendgentoo/sendgentoo/gpart_make_hybrid_mbr.exp "${device}" || exit 1
-echo "making hybrid MBR exited 0, should be good to go."
+echo "$0: making hybrid MBR exited 0, should be good to go."

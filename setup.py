1b1ef247fdb1789e80b068e810cac3f28b7ad553
jakeogh <github.com@v6y.net>
2022-10-26 18:06:01 -0700

    auto-commit

--- setup.py
+++ setup.py
@@ -2,33 +2,31 @@
 """
 sendgentoo
 """
-import sys
-if not sys.version_info[0] == 3:
-    sys.exit("Sorry, Python 3 is required. Use: \'python3 setup.py install\'")
 
-import re
-from setuptools import find_packages, setup
+from setuptools import find_packages
+from setuptools import setup
+
 dependencies = []
 version = 0.1
 
 setup(
-    name = "sendgentoo",
-    version = version,
-    url = "https://github.com/jakeogh/sendgentoo",
-    license='MIT',
-    author = "jakeogh",
-    author_email = "github.com@v6y.net",
-    description='Install Gentoo Linux',
+    name="sendgentoo",
+    version=version,
+    url="https://github.com/jakeogh/sendgentoo",
+    license="MIT",
+    author="jakeogh",
+    author_email="github.com@v6y.net",
+    description="Install Gentoo Linux",
     long_description=__doc__,
-    packages=find_packages(exclude=['tests']),
+    packages=find_packages(exclude=["tests"]),
     include_package_data=True,
     zip_safe=False,
-    platforms='any',
+    platforms="any",
     install_requires=dependencies,
     entry_points={
-        'console_scripts': [
-            'sendgentoosimple = sendgentoo.sendgentoosimple:sendgentoosimple',
-            'sendgentoo = sendgentoo.sendgentoo:sendgentoo',
+        "console_scripts": [
+            "sendgentoosimple = sendgentoo.sendgentoosimple:sendgentoosimple",
+            "sendgentoo = sendgentoo.sendgentoo:sendgentoo",
         ],
     },
     classifiers=[
@@ -36,19 +34,18 @@ setup(
         # 'Development Status :: 1 - Planning',
         # 'Development Status :: 2 - Pre-Alpha',
         # 'Development Status :: 3 - Alpha',
-        'Development Status :: 4 - Beta',
+        "Development Status :: 4 - Beta",
         # 'Development Status :: 5 - Production/Stable',
         # 'Development Status :: 6 - Mature',
         # 'Development Status :: 7 - Inactive',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Operating System :: POSIX',
-        'Operating System :: MacOS',
-        'Operating System :: Unix',
-        'Operating System :: Windows',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ]
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Operating System :: POSIX",
+        "Operating System :: MacOS",
+        "Operating System :: Unix",
+        "Operating System :: Windows",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
 )

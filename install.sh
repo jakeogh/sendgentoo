d61cf0e2f103e7cf1be5f2bf0b155d3c163f1a81
jakeogh <github.com@v6y.net>
2023-07-08 10:41:15 -0700

    auto-commit

--- install.sh
+++ install.sh
@@ -16,3 +16,4 @@ eselect repository add jakeogh git https://github.com/jakeogh/jakeogh
 emerge --sync
 CONFIG_PROTECT="-*" emerge --autounmask --autounmask-write --autounmask-continue sendgentoo
 sendgentoosimple --help
+
